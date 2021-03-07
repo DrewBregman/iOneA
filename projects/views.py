9a50374fd20d33c3735d2e9fa0edc72e5eddfc1b
andrew-bregman <andrew.bregman@westpoint.edu>
2021-03-06 21:15:19 -0500

    commit on 'master'

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,10 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 
-#from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView
 
-#from .models import Project
+from .models import Project
 
 
-#class HomePageView(TemplateView):
-    #template_name = 'home.html'
-
-#class SearchResultsView(ListView):
-    #model = Project
-    #template_name = 'search_results.html'
+class SearchResultsView(ListView):
+    model = Project
+    template_name = 'search_results.html'
