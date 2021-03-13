28298bdac271a4bbe041f214180bad35fb841e80
thesethb <67613424+thesethb@users.noreply.github.com>
2021-03-13 15:30:02 -0500

    some changes

--- Notifications/models.py
+++ Notifications/models.py
@@ -6,5 +6,6 @@ from django.contrib.auth.models import User
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.CharField(max_length=200, default='notification')
+    url = models.CharField(max_length = 500, blank='', default='')
     def __str__(self):
         return self.message
--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=30)
@@ -7,7 +7,7 @@ class Project(models.Model):
     numUser=models.IntegerField()
     bPic = models.ImageField(
         upload_to ='uploads', blank=False)
-
+    Leader = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
     purpose=models.TextField()
     tag_choice = (
         ('Data Analysis' , ('Data Analysis')),
