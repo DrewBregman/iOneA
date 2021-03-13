0c12aefe1da1e7f9f3b486b80dfeb8db93ab528f
andrew-bregman <andrew.bregman@westpoint.edu>
2021-03-13 15:40:42 -0500

    updated project

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=30)
     department=models.CharField(max_length=30)
     numUser=models.IntegerField()
-    bPic = models.ImageField(
-        upload_to ='uploads', blank=False)
-    Leader = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+    bPic = models.ImageField(default='defaultproban.jpg', upload_to='project_banner')
+    logo = models.ImageField(default='defaultlogo.jpg', upload_to='project_logo')
+    dep_choice1 = (
+        ('Behavioral Sciences and Leadership', ('Behavioral Sciences and Leadership')),
+        ('Chemistry and Life Science', ('Chemistry and Life Science')),
+        ('Civil and Mechanical Engineering', ('Civil and Mechanical Engineering')),
+        ('Electrical Engineering and Comptuer Science', ('Electrical Engineering and Comptuer Science')),
+        ('English and Philosophy', ('English and Philosophy')),
+        ('Foreign Languages', ('Foreign Languages')),
+        ('Geography and Environmental Engineering', ('Geography and Environmental Engineering')),
+        ('History', ('History')),
+        ('Law', ('Law')),
+        ('Mathematical Sciences', ('Mathematical Sciences')),
+        ('Physics and Nuclear Engineering', ('Physics and Nuclear Engineering')),
+        ('Social Sciences', ('Social Sciences')),
+        ('Systems Engineering', ('Systems Engineering')),
+        ('Independent', ('Independent')),
+    )
+    projectDepartment = models.CharField(
+        max_length=50,
+        choices=dep_choice1,
+        default='Independent',
+    )
+
     purpose=models.TextField()
     tag_choice = (
         ('Data Analysis' , ('Data Analysis')),
@@ -22,6 +45,22 @@ class Project(models.Model):
         choices=tag_choice,
         default='Frauds',
     )
+
+    look = (
+        ('motivated cadets with niche expertise.', ('Expert Cadets')),
+        ('cadets who want to learn and help.', ('Any cadet who wants to help')),
+        ('an engineering cadet.', ('Engineering Cadet')),
+        ('a cadet with a scientific background.', ('Scientific background')),
+        ('cadets with programming experience.', ('Coding Background')),
+       
+    )
+    
+    lookingFor = models.CharField(
+        max_length=75,
+        choices=look,
+        default='an engineering cadet,',
+    )
+ 
 
     class Meta:
         verbose_name_plural= "projects"
